f4f7b42fe3a4ce999bd8f8dd41b03a0bf75af494
1111-M <44734807+1111-M@users.noreply.github.com>
2018-11-07 20:56:33 -0800

    Update twitterbot.js

--- src/twitterbot.js
+++ src/twitterbot.js
@@ -9,22 +9,22 @@ stream.on('direct_message', function (eventMsg) {
     var screenName = eventMsg.direct_message.sender.screen_name;
     var msgID = eventMsg.direct_message.id_str;
 
-    if (screenName === my_user_name) {
+    if (screenName === 'my_user_name') {
         return callbackHandler(msgID);
     }
 
-    else if (msg.search('edmonton','calgary') !== -1 ) {
+    else if (msg.search['Hi','Hello'] !== -1 ) {
         return T.post('direct_messages/new', { 
             screen_name: screenName,
-            text: 'Hey Alberta!'} , function () {
+            text: 'Hey, what can I do for you?'} , function () {
             callbackHandler(msgID);
         });
     }
 
-    else if (msg.search('Whistler','Vancouver') !== -1 ) {
+    else if (msg.search[‘Apple’,’Red'] !== -1 ) {
         return T.post('direct_messages/new', { 
             screen_name: screenName,
-            text: ‘Love it! BC’} , function () {
+            text: ‘Love it!’} , function () {
             callbackHandler(msgID);
         });
     }
@@ -32,8 +32,9 @@ stream.on('direct_message', function (eventMsg) {
     else {
         return T.post('direct_messages/new', {
             screen_name: screenName,
-            text: "sorry i cant tell where you are from"
+            text: "I don't know "
         }, function() {
             callbackHandler(msgID);
         });
-    });
+    }
+});
