45bd896315a9d30d3bdeed343b20753b37091644
1111-M <44734807+1111-M@users.noreply.github.com>
2018-11-09 23:15:51 -0800

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -1,34 +1,30 @@
 const T = require("./Twit.js");
+
 const my_user_name = require("../config").userName;
 //const timeout = 1000 * 60 * 5; // timeout to send the message 5 min
 
+var stream = T.stream('user');
 
 const AutoDM = () => {
-
-  //get the user stream
-  var stream = T.stream('user');
-
-stream.on('direct_message', function (eventMsg) {
+stream.on('direct_message', function (eventMsg) 
   var msg = eventMsg.direct_message.text;
   var screenName = eventMsg.direct_message.sender.screen_name;
-  var userId = eventMsg.direct_message.sender.id;
+  var msgID = eventMsg.direct_message.id_str;
 
-  // reply object
-  var replyTo = { user_id: userId,
-    text: "Thanks for your message YAAAAAAS!", 
-    screen_name: screenName };
+  if (msg.search('#test') !== -1) {
+    return T.post('statuses/update', { status: msg}, function () {
+      console.log('I tweeted the message');
+    });
+  }
 
-  console.log(screenName + " says: " + msg );
-
-  // avoid replying to yourself when the recipient is you
-  if(screenName != my_user_name){
-
-    //post reply
-    T.post("direct_messages/new",replyTo, function(err,data,response){
-            console.info(data);
-        });
-    }
+  else if (screen_name != my_user_name){
+    return T.post('direct_messages/new', {
+      screen_name: screenName,
+      text: 'YAAAAS!'
+    }, function () {
+      console.log('Successfully replied YAAAAS');
+    });
 });
-}
 
+}
 module.exports = AutoDM;
