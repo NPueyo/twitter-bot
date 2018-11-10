b3d87bf18e1b28c75b3eb42394a71d81b57f83bf
1111-M <44734807+1111-M@users.noreply.github.com>
2018-11-09 21:38:37 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const twitterApp = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_key,
+  access_token_secret: process.env.access_token_secret,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
   twitterApp,
-  userName: process.env.USERNAME
+  userName: process.env.username
 };
